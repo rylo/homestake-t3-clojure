cfdaf1684e3ba72a0f29ceb92a8e2fa84ebb7cec
Rylan Dirksen <rylandirksen@gmail.com>
2013-03-25 14:01:21 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to homestake-t3-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/homestake_t3_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns homestake-t3-clojure.core-test
+  (:use clojure.test
+        homestake-t3-clojure.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
